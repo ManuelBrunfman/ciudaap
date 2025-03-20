227af254397baa349af4f0bd746963eee631d91f
Manuel Brunfman <manubrunfman.dev@gmail.com>
2025-03-20 09:32:53 -0300

    empece aver soluciones de gimini para el deploy

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -14,6 +14,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const firestore = getFirestore(app);
-export const storage = getStorage(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, firestore, storage };
